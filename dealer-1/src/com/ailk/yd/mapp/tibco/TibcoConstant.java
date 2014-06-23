6fbd516281f18f7ecbc0edce34b2cc98e36b198f
qianshihua <qianshihua@gmail.com>
2014-06-23 16:43:36 +0530

    证件类型

--- dealer-1/src/com/ailk/yd/mapp/tibco/TibcoConstant.java
+++ dealer-1/src/com/ailk/yd/mapp/tibco/TibcoConstant.java
@@ -80,6 +80,50 @@ public class TibcoConstant {
 	public static String TIBCO_ServiceType_LTEMobility = "Z0002";
 	public static String TIBCO_ServiceType_LTEVoice = "Z0003";
 	public static String TIBCO_ServiceType_LTEData = "Z0004";
+	
+	
+	public static String TIBCO_IDENTIFICATIONTYPE_VISA="Z00004";
+	public static String TIBCO_IDENTIFICATIONTYPE_AadhaarCard="Z00005";
+	public static String TIBCO_IDENTIFICATIONTYPE_CustomerPictureURLId="Z00006";
+	public static String TIBCO_IDENTIFICATIONTYPE_ElectionCommissionIDcard="Z00008";
+	public static String TIBCO_IDENTIFICATIONTYPE_CGHSECHScard="Z00009";
+	public static String TIBCO_IDENTIFICATIONTYPE_CertificateofAddresshavingphotoissuedbyMPorMLAorGazettedOfficerorTehsildaronletterhead="Z00010";
+	public static String TIBCO_IDENTIFICATIONTYPE_Certificateofphotoidentity,issuedbyVillagePanchayatheadoritsequivalentauthorityforruralareas="Z00012";
+	public static String TIBCO_IDENTIFICATIONTYPE_PhotoCreditcard="Z00013";
+	public static String TIBCO_IDENTIFICATIONTYPE_SmartcardissuedbyCSD,DefenceParamilitary="Z00015";
+	public static String TIBCO_IDENTIFICATIONTYPE_CurrentPassbookofPostOfficeanyscheduledbankhavingPhoto="Z00016";
+	public static String TIBCO_IDENTIFICATIONTYPE_PhotoIdentitycardofCentralGovtPSUorStateGovtPSUonly="Z00017";
+	public static String TIBCO_IDENTIFICATIONTYPE_PhotoIdentityCardissuedbyGovtrecognizededucationalinstitutionsforstudentsonly="Z00022";
+	public static String TIBCO_IDENTIFICATIONTYPE_UIDAIletter="Z00023";
+	public static String TIBCO_IDENTIFICATIONTYPE_RILEmployeeIdentityCard="Z00024";
+	public static String TIBCO_IDENTIFICATIONTYPE_BankStatementPassbook="Z00025";
+	public static String TIBCO_IDENTIFICATIONTYPE_PostOfficeAccountStatementPassbook="Z00026";
+	public static String TIBCO_IDENTIFICATIONTYPE_RationCard="Z00027";
+	public static String TIBCO_IDENTIFICATIONTYPE_VoterID="Z00028";
+	public static String TIBCO_IDENTIFICATIONTYPE_GovernmentPhotoIDcardsservicephotoidentitycardissuedbyPSU="Z00029";
+	public static String TIBCO_IDENTIFICATIONTYPE_ElectricityBillnotolderthan3months="Z00030";
+	public static String TIBCO_IDENTIFICATIONTYPE_Waterbillnotolderthan3months="Z00031";
+	public static String TIBCO_IDENTIFICATIONTYPE_TelephoneLandlineBillnotolderthan3months="Z00032";
+	public static String TIBCO_IDENTIFICATIONTYPE_PropertyTaxReceiptnotolderthan3months="Z00033";
+	public static String TIBCO_IDENTIFICATIONTYPE_CreditCardStatementnotolderthan3months="Z00034";
+	public static String TIBCO_IDENTIFICATIONTYPE_InsurancePolicy="Z00035";
+	public static String TIBCO_IDENTIFICATIONTYPE_SignedLetterhavingPhotofromBankonletterhead="Z00036";
+	public static String TIBCO_IDENTIFICATIONTYPE_SignedLetterhavingPhotoissuedbyregisteredCompanyonletterhead="Z00037";
+	public static String TIBCO_IDENTIFICATIONTYPE_SignedLetterhavingPhotoissuedbyRecognizedEducationalInstructiononletterhead="Z00038";
+	public static String TIBCO_IDENTIFICATIONTYPE_NREGSJobCard="Z00039";
+	public static String TIBCO_IDENTIFICATIONTYPE_ArmsLicense="Z00040";
+	public static String TIBCO_IDENTIFICATIONTYPE_PensionerCard="Z00041";
+	public static String TIBCO_IDENTIFICATIONTYPE_FreedomFighterCard="Z00042";
+	public static String TIBCO_IDENTIFICATIONTYPE_havingPhotoPOI="Z00043";
+	public static String TIBCO_IDENTIFICATIONTYPE_PensionerCardhavingphoto="Z00044";
+	public static String TIBCO_IDENTIFICATIONTYPE_RegisteredSaleLeaseRentAgreement="Z00045";
+	public static String TIBCO_IDENTIFICATIONTYPE_AddressCardhavingPhotoissuedbyDepartmentofPosts="Z00046";
+	public static String TIBCO_IDENTIFICATIONTYPE_CasteandDomicileCertificatehavingPhotoissuedbyStateGovt="Z00047";
+	public static String TIBCO_IDENTIFICATIONTYPE_DisabilityIDCardhandicappedmedicalcertificateissuedbytherespectiveStateUTGovernmentsAdministrations="Z00048";
+	public static String TIBCO_IDENTIFICATIONTYPE_GasConnectionBillnotolderthan3months="Z00049";
+	public static String TIBCO_IDENTIFICATIONTYPE_PassportofSpouse="Z00050";
+	public static String TIBCO_IDENTIFICATIONTYPE_PassportofParentsincaseofMinor="Z00051";
+
 
 	/**
 	 * 将该类中的静态变量封装成终端要用的格式。 [{MARITALSTATUS={Divorced=3, SINGLE=0, Separated=4,
