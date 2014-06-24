f0bf2aa701a6312e0bf14c09cc02d2c6cb5250ec
qianshihua <qianshihua@gmail.com>
2014-06-24 22:56:25 +0530

    37接口多个订单编号

--- dealer-1/src/com/ailk/yd/mapp/client/model/HW0037Response.java
+++ dealer-1/src/com/ailk/yd/mapp/client/model/HW0037Response.java
@@ -1,17 +1,62 @@
 package com.ailk.yd.mapp.client.model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import com.ailk.yd.mapp.model.YDBody;
 
 public class HW0037Response extends YDBody {
 
-	private String transactionRefNumber;
-
-	public String getTransactionRefNumber() {
-		return transactionRefNumber;
+	
+	private List<TracRefNum> tracRefNums;
+	
+	
+	public void addTracRef(TracRefNum trn){
+		if(tracRefNums==null){
+			tracRefNums = new ArrayList();
+		}
+		this.tracRefNums.add(trn);
+	}
+	
+	public static class TracRefNum{
+		private String tracType;
+		private Set<String> tracNums;
+		public String getTracType() {
+			return tracType;
+		}
+		public void setTracType(String tracType) {
+			this.tracType = tracType;
+		}
+		public Set<String> getTracNums() {
+			return tracNums;
+		}
+		public void setTracNums(Set<String> tracNums) {
+			this.tracNums = tracNums;
+		}
+		public TracRefNum(String tracType) {
+			super();
+			this.tracType = tracType;
+		}
+		public void addTracNum(String tracNum){
+			if(tracNums==null){
+				tracNums = new HashSet();
+			}
+			tracNums.add(tracNum);
+		}
+		
+		
+		
 	}
 
-	public void setTransactionRefNumber(String transactionRefNumber) {
-		this.transactionRefNumber = transactionRefNumber;
+	public List<TracRefNum> getTracRefNums() {
+		return tracRefNums;
 	}
+
+	public void setTracRefNums(List<TracRefNum> tracRefNums) {
+		this.tracRefNums = tracRefNums;
+	}
+
 	
 }
