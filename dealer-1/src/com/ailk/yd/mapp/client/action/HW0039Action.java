fbc676bf4b4ced56f7697ed9ab44d7e903e4dafc
qianshihua <qianshihua@gmail.com>
2014-06-24 22:04:43 +0530

    39jiekou

---
+++ dealer-1/src/com/ailk/yd/mapp/client/action/HW0039Action.java
@@ -0,0 +1,43 @@
+package com.ailk.yd.mapp.client.action;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ai.mapp.sys.entity.AccountInfo;
+import com.ai.mapp.sys.entity.User;
+import com.ai.mapp.sys.service.UserService;
+import com.ailk.butterfly.core.exception.BusinessException;
+import com.ailk.butterfly.core.exception.SystemException;
+import com.ailk.butterfly.mapp.core.annotation.Action;
+import com.ailk.yd.mapp.client.model.HW0039Request;
+import com.ailk.yd.mapp.client.model.HW0039Response;
+
+/**
+ * 取登录账户的余额
+ * @author qianshihua
+ *
+ */
+@Service("hw0039")
+@Action(bizcode="hw0039",userCheck=true)
+public class HW0039Action extends
+		AbstractYDBaseActionHandler<HW0039Request, HW0039Response> {
+
+	@Autowired
+	private UserService userService;
+	@Override
+	protected void doAction() throws BusinessException, SystemException,
+			Exception {
+		String userCode = this.getUserinfo().getUserName();
+		User user = userService.loadUserByUserCode(userCode);
+		List<AccountInfo> accounts = user.getAccounts();
+		if(accounts==null || accounts.size()<1){
+			throw new Exception("Dealer: "+userCode+" has no account!");
+		}
+		Long amount = accounts.get(0).getAmount();
+		this.response=new HW0039Response();
+		this.response.setAmount(amount.toString());
+	}
+
+}
---
+++ dealer-1/src/com/ailk/yd/mapp/client/model/HW0039Response.java
@@ -0,0 +1,17 @@
+package com.ailk.yd.mapp.client.model;
+
+import com.ailk.yd.mapp.model.YDBody;
+
+public class HW0039Response extends YDBody {
+
+	private String amount;
+
+	public String getAmount() {
+		return amount;
+	}
+
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	
+}
