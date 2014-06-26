8d1c7fffa5088941bf4d878f6a067072c8d3f93f
luyang <luyiq@163.com>
2014-06-26 16:33:12 +0530

    0008Action

--- dealer-1/src/com/ailk/yd/mapp/tibco/action/YD0008Action.java
+++ dealer-1/src/com/ailk/yd/mapp/tibco/action/YD0008Action.java
@@ -70,23 +70,6 @@ public class YD0008Action extends AbstractTibcoService<YD0008Request, YD0008Resp
 //		JSONObject.fromObject(m).toString();
 //		return JSONObject.fromObject(m).toString();
 	}
-	
-	public static void main(String[] args) throws Exception {
-		YD0008Action yd8 = new YD0008Action();
-		YD0008Request req =  new YD0008Request();
-		YdPage pg = new YdPage();
-		req.setPaging(pg);
-		req.setPattern("1234");
-		pg.setPageSize("10");
-		pg.setOffset("5");
-		req.setCiecleId("TC");
-		System.err.println(yd8.convertRequest(req));
-		System.err.println(new ObjectMapper().writeValueAsString(req));
-		
-		YD0008Response res = yd8.convertResponse("{\"number\":[{\"id\":\"3334021234\"},{\"id\":\"3334041234\"},{\"id\":\"3334061234\"}],\"vanityName\":\"\"}");
-		res = yd8.convertResponse("{ \"number\":[ ] , \"vanityName\":\"\" }");
-		
-	}
 
 
 	@Override
