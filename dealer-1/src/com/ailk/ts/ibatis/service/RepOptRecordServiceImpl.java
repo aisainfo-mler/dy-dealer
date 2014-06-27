611d90ccf0250dadbbfa24fd08d454ae2ffaabed
tanglian <390565541@qq.com>
2014-06-27 15:30:38 +0800

    库存

---
+++ dealer-1/src/com/ailk/ts/ibatis/service/RepOptRecordServiceImpl.java
@@ -0,0 +1,67 @@
+package com.ailk.ts.ibatis.service;
+
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ailk.butterfly.core.exception.BusinessException;
+import com.ailk.butterfly.core.exception.SystemException;
+import com.ailk.ts.dal.ibatis.RepOptRecordDAO;
+import com.ailk.ts.dal.ibatis.model.RepOptRecord;
+import com.ailk.ts.dal.ibatis.model.RepOptRecordExample;
+import com.ailk.ts.dal.ibatis.model.RepOptRecordExample.Criteria;
+
+@Service
+@Transactional(rollbackFor=Exception.class)
+public class RepOptRecordServiceImpl{
+
+	@Autowired
+	private RepOptRecordDAO repOptRecordDao;
+
+	public int countRepOptRecord(RepOptRecord ror) throws BusinessException,
+			SystemException {
+		return this.repOptRecordDao.countByExample(this.convert2Example(ror));
+	}
+
+	public List<RepOptRecord> getRep(RepOptRecord ror, int start, int limit)
+			throws BusinessException, SystemException {
+		// TODO Auto-generated method stub
+		RepOptRecordExample e = this.convert2Example(ror);
+		e.setLimitClauseStart(start);
+		e.setLimitClauseCount(limit);
+		return this.repOptRecordDao.selectByExample(e);
+	}
+
+	private RepOptRecordExample convert2Example(RepOptRecord ror) {
+		RepOptRecordExample e = new RepOptRecordExample();
+		Criteria cc = e.createCriteria();
+		if (ror.getSkuid() != null) {
+			cc.andSkuidEqualTo(ror.getSkuid());
+		}
+		if (StringUtils.isNotBlank(ror.getInputRepCode())) {
+			cc.andInputRepCodeEqualTo(ror.getInputRepCode());
+		}
+		if (StringUtils.isNotBlank(ror.getOutputRepCode())) {
+			Criteria cc2 = e.or();
+			if (ror.getSkuid() != null) {
+				cc2.andSkuidEqualTo(ror.getSkuid());
+			}
+			if (StringUtils.isNotBlank(ror.getOutputRepCode())) {
+				cc2.andOutputRepCodeEqualTo(ror.getOutputRepCode());
+			}
+		}
+		return e;
+	}
+
+	public RepOptRecordDAO getRepOptRecordDao() {
+		return repOptRecordDao;
+	}
+
+	public void setRepOptRecordDao(RepOptRecordDAO repOptRecordDao) {
+		this.repOptRecordDao = repOptRecordDao;
+	}
+
+}
---
+++ dealer-1/src/com/ailk/ts/ibatis/service/RepSellDetailServiceImpl.java
@@ -0,0 +1,74 @@
+package com.ailk.ts.ibatis.service;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ai.mapp.sys.util.SYSConstant;
+import com.ailk.butterfly.core.exception.BusinessException;
+import com.ailk.butterfly.core.exception.SystemException;
+import com.ailk.ts.dal.ibatis.RepSellDetailDAO;
+import com.ailk.ts.dal.ibatis.model.RepSellDetail;
+import com.ailk.ts.dal.ibatis.model.RepSellDetailExample;
+
+@Service
+@Transactional(rollbackFor=Exception.class)
+public class RepSellDetailServiceImpl {
+	
+	private static final Logger log = LoggerFactory.getLogger(RepSellDetailServiceImpl.class);
+	
+	@Autowired
+	private RepSellDetailDAO repSellDetailDAO;
+
+	/**
+	 * <p>描述: 对于加盟商出售DEVICE  是没有库存操作记录ID的</p> 
+	 * @param detail
+	 * @param optId
+	 * @return
+	 * @throws BusinessException
+	 * @throws SystemException    
+	 * @return:       Integer    
+	 * @Date          2013-5-13 下午05:12:45
+	 */
+	public Integer createSecondSellDetail(Integer orderId, Integer entityId,
+			Integer optId,String optType) throws BusinessException, SystemException {
+		RepSellDetail detail = new RepSellDetail();
+		detail.setEntityId(entityId);
+		detail.setOpterId(optId);
+		detail.setOptTime(new Timestamp(System.currentTimeMillis()));
+		detail.setOrderId(orderId);
+		detail.setOptType(optType);
+		detail.setOrderDomain(SYSConstant.ORDER_DOMAIN_ORDER);
+		return repSellDetailDAO.insertSelective(detail);
+	}
+
+	/**
+	 * 寻找一个商品的相关库存操作记录信息
+	 */
+	public List<RepSellDetail> findRepSellDetailByEntityId(Integer entityId)throws BusinessException, SystemException {
+		RepSellDetailExample detail_ex = new RepSellDetailExample();
+		RepSellDetailExample.Criteria criteria = detail_ex.createCriteria();
+		criteria.andEntityIdEqualTo(entityId);
+		detail_ex.setOrderByClause("OPT_TIME desc");
+		return repSellDetailDAO.selectByExample(detail_ex);
+	}
+	
+	
+	/**
+	 * 寻找一个商品的最近库存操作记录信息
+	 */
+	public RepSellDetail findLastByRepSellDetailEntityId(Integer entityId)throws BusinessException, SystemException {
+		List<RepSellDetail> list = findRepSellDetailByEntityId(entityId);
+		if(list != null && list.isEmpty() == false){
+				return list.get(0);
+		}else{
+			return null;
+		}
+		
+	}
+}
