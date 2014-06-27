d7b7c47d8943f506b128768ef79387a64e6cb079
zhengwj <wengjuan025@163.com>
2014-06-27 15:39:26 +0800

    仓库

---
+++ dealer-1/src/com/ailk/ts/dal/ibatis/CommonQueryDAO.java
@@ -0,0 +1,20 @@
+package com.ailk.ts.dal.ibatis;
+
+import java.util.List;
+
+
+public interface CommonQueryDAO {
+
+	public List getModels(Object obj,String sqlKey);
+	public List getNewUserReportList(Object obj, String sqlKey);
+	
+	public Object findByPkey(java.lang.String serialno, String sqlKey);
+	
+	public void addModel(Object object, String sqlKey);
+	public void modModel(Object object, String sqlKey);
+	public void delModel(java.lang.String serialno, String sqlKey);
+
+  
+	public Object findModel(Object object, String sqlKey);
+	public Object getRealName(Object obj, String sqlKey);
+}
