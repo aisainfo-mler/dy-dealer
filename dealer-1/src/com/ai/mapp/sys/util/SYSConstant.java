8c6a47c053610922bf4c6955d39a7902edec46b5
qianshihua <qianshihua@gmail.com>
2014-06-30 21:16:32 +0530

    me

--- dealer-1/src/com/ai/mapp/sys/util/SYSConstant.java
+++ dealer-1/src/com/ai/mapp/sys/util/SYSConstant.java
@@ -14,7 +14,7 @@ import com.ai.mapp.sys.entity.AgentOrder;
 import com.ai.mapp.sys.entity.FileManageCondition;
 
 
-public class SYSConstant { 
+public class SYSConstant {
 	/** 默认的MAPP系统应用key **/
 	public static final String MAPP_APPLICATION_PRODUCTKEY = "MAPP";
 	/** 超级管理员角色 **/
@@ -447,11 +447,11 @@ public class SYSConstant {
 	/**
 	 * 充值
 	 */
-	public static final String AGENT_ORDER_TYPE_TOPUP="2";
+	public static final String AGENT_ORDER_TYPE_RECHARGE="2";
 	/**
-	 * addon 
+	 * topup
 	 */
-	public static final String AGENT_ORDER_TYPE_ADDON="4";
+	public static final String AGENT_ORDER_TYPE_TOPUP="4";
 	/**
 	 * 补卡
 	 */
@@ -461,9 +461,9 @@ public class SYSConstant {
 	 */
 	public static final String ORDER_TYPE_MOBILE_SIM ="4";//买手机或卡
 	/**
-	 * recharge
+	 * 叠加包
 	 */
-	public static final String AGENT_ORDER_TYPE_RECHARGE = "5";
+	public static final String AGENT_ORDER_TYPE_BOLT_ON = "5";
 	
 	
 	/**
@@ -693,19 +693,19 @@ public class SYSConstant {
 		
 		/** 代理商订单类型 **/
 		agentOrderTypesL.add(AGENT_ORDER_TYPE_NEW);
-		agentOrderTypesL.add(AGENT_ORDER_TYPE_TOPUP);
-		agentOrderTypesL.add(AGENT_ORDER_TYPE_SIMCARD);
 		agentOrderTypesL.add(AGENT_ORDER_TYPE_RECHARGE);
+		agentOrderTypesL.add(AGENT_ORDER_TYPE_SIMCARD);
+		agentOrderTypesL.add(AGENT_ORDER_TYPE_BOLT_ON);
 		
 		agentOrderTypes.put(SYSConstant.AGENT_ORDER_TYPE_NEW + LANGUAGE_ENGLISH, "New User");
-		agentOrderTypes.put(SYSConstant.AGENT_ORDER_TYPE_TOPUP + LANGUAGE_ENGLISH, "Top Up");//充值
+		agentOrderTypes.put(SYSConstant.AGENT_ORDER_TYPE_RECHARGE + LANGUAGE_ENGLISH, "Top Up");//充值
 		agentOrderTypes.put(SYSConstant.AGENT_ORDER_TYPE_SIMCARD + LANGUAGE_ENGLISH, "SIM Care");
-		agentOrderTypes.put(SYSConstant.AGENT_ORDER_TYPE_RECHARGE + LANGUAGE_ENGLISH, "Add-On");//叠加包
+		agentOrderTypes.put(SYSConstant.AGENT_ORDER_TYPE_BOLT_ON + LANGUAGE_ENGLISH, "Add-On");//叠加包
 		
 		agentOrderTypes.put(SYSConstant.AGENT_ORDER_TYPE_NEW + LANGUAGE_CHINA, "开户");
-		agentOrderTypes.put(SYSConstant.AGENT_ORDER_TYPE_TOPUP + LANGUAGE_CHINA, "充值");//充值
+		agentOrderTypes.put(SYSConstant.AGENT_ORDER_TYPE_RECHARGE + LANGUAGE_CHINA, "充值");//充值
 		agentOrderTypes.put(SYSConstant.AGENT_ORDER_TYPE_SIMCARD + LANGUAGE_CHINA, "补换卡");
-		agentOrderTypes.put(SYSConstant.AGENT_ORDER_TYPE_RECHARGE + LANGUAGE_CHINA, "叠加包");//叠加包
+		agentOrderTypes.put(SYSConstant.AGENT_ORDER_TYPE_BOLT_ON + LANGUAGE_CHINA, "叠加包");//叠加包
 		
 		/** 代理商订单状态 **/
 //		* Dealer 销售订单 状态 Sales Order Status
@@ -735,9 +735,9 @@ public class SYSConstant {
 		
 		/** 订单折扣定义 **/
 		discountTypes.put(SYSConstant.AGENT_ORDER_TYPE_SIMCARD, Float.valueOf("5"));
-		discountTypes.put(SYSConstant.AGENT_ORDER_TYPE_TOPUP, Float.valueOf("2"));
+		discountTypes.put(SYSConstant.AGENT_ORDER_TYPE_RECHARGE, Float.valueOf("2"));
 		discountTypes.put(SYSConstant.AGENT_ORDER_TYPE_NEW, Float.valueOf("10"));
-		discountTypes.put(SYSConstant.AGENT_ORDER_TYPE_RECHARGE, Float.valueOf("10"));
+		discountTypes.put(SYSConstant.AGENT_ORDER_TYPE_BOLT_ON, Float.valueOf("10"));
 		
 		
 		/**支付状态**/
