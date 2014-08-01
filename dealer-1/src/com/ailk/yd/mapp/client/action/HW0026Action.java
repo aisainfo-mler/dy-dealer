c6a3f7e844b59e1bb2799fe03a88ce5072f2ae81
zhengwj <crystal0579zwj@gmail.com>
2014-08-01 21:06:35 +0800

    sysout

--- dealer-1/src/com/ailk/yd/mapp/client/action/HW0026Action.java
+++ dealer-1/src/com/ailk/yd/mapp/client/action/HW0026Action.java
@@ -1,6 +1,8 @@
 package com.ailk.yd.mapp.client.action;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 import com.ai.mapp.base.StringUtil;
 import com.ai.mapp.base.entity.ResultJson;
@@ -9,6 +11,7 @@ import com.ai.mapp.sys.service.AgentOrderService;
 import com.ai.mapp.sys.service.OrderInfoService;
 import com.ailk.butterfly.core.exception.BusinessException;
 import com.ailk.butterfly.core.exception.SystemException;
+import com.ailk.butterfly.mapp.core.annotation.Action;
 import com.ailk.yd.mapp.client.model.HW0026Request;
 import com.ailk.yd.mapp.client.model.HW0026Response;
 
@@ -17,7 +20,9 @@ import com.ailk.yd.mapp.client.model.HW0026Response;
  * @version 创建时间：2014-8-1 下午03:28:12
  * 类说明:
  */
-
+@Service("hw0026")
+@Action(bizcode = "hw0026", userCheck = false)
+@Scope("prototype")
 public class HW0026Action extends AbstractYDBaseActionHandler<HW0026Request, HW0026Response> {
 	@Autowired
 	private AgentOrderService agentOrderService; 
